ba87456815e8d368e958c85ab7a483015d9214a2
cfly <colin.flynn@ge.com>
2018-12-06 17:14:43 -0500

    added threads for lights to light in background.  Page loading doesnt wait for lights

--- app.py
+++ app.py
@@ -1,39 +1,60 @@
 from flask import Flask, render_template
 from src.lightController import LightController
+import threading
 import json
 
 app = Flask(__name__)
 lightController = LightController()
 
 @app.route('/')
-def index():
-    lightController.lightsWhite()
+def off():
+    lightController.lightsOff()
+    return render_template('index.html', current_color='No')
+
+@app.route('/white')
+def white():
+    thread = threading.Thread(target=whiteThread)
+    thread.start()
     return render_template('index.html', current_color='White')
 
 @app.route('/red')
 def red():
-    lightController.lightsRed()
+    thread = threading.Thread(target=redThread)
+    thread.start()
     return render_template('index.html', current_color='Red')
-
-@app.route('/rainbow')
-def rainbow():
-    lightController.lightsRainbow()
-    return render_template('index.html', current_color='Rainbow')
 
 @app.route('/redwhite')
 def redWhite():
-    lightController.lightsRedWhite()
+    thread = threading.Thread(target=redWhiteThread)
+    thread.start()
     return render_template('index.html', current_color='Red and White')
 
 @app.route('/redwhitegreen')
 def redWhiteGreen():
-    lightController.lightsRedWhiteGreen()
+    thread = threading.Thread(target=redWhiteGreenThread)
+    thread.start()
     return render_template('index.html', current_color='Red White and Green')
 
-@app.route('/off')
-def off():
-    lightController.lightsOff()
-    return render_template('index.html', current_color='No')
+@app.route('/rainbow')
+def rainbow():
+    thread = threading.Thread(target=rainbowThread)
+    thread.start()
+    return render_template('index.html', current_color='Rainbow')
+
+def whiteThread():
+    lightController.lightsWhite()
+
+def redThread():
+    lightController.lightsRed()
+
+def redWhiteThread():
+    lightController.lightsRedWhite()
+
+def redWhiteGreenThread():
+    lightController.lightsRedWhite()
+
+def rainbowThread():
+    lightController.lightsRainbow()
 
 try:
     app.run(debug=True,use_reloader=False,host='0.0.0.0')
